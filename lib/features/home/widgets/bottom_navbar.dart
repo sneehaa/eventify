330f3b048ad0adc0736c7b8111977b5995870643
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-06-22 16:31:29 +0545

    updated the bottom nav bar

--- lib/features/home/widgets/bottom_navbar.dart
+++ lib/features/home/widgets/bottom_navbar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({Key? key}) : super(key: key);
+  const BottomNavBar({super.key});
 
   @override
   _BottomNavBarState createState() => _BottomNavBarState();
