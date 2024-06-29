1f83b4266e891220a570db93c322e338775d403f
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-06-29 14:31:26 +0545

    updated app_router.dart

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,8 +1,12 @@
+import 'package:eventify/features/createEvent/create_event.dart';
 import 'package:eventify/features/details/checkout.dart';
 import 'package:eventify/features/details/details_screen.dart';
+import 'package:eventify/features/favorites/favorites.dart';
 import 'package:eventify/features/home/home_screen.dart';
 import 'package:eventify/features/login/login.dart';
 import 'package:eventify/features/onBoarding/onboarding_screen.dart';
+import 'package:eventify/features/profile/edit_profile.dart';
+import 'package:eventify/features/profile/user_profile.dart';
 import 'package:eventify/features/signup/signup.dart';
 import 'package:eventify/features/splash/splash_screen.dart';
 
@@ -13,22 +17,28 @@ class AppRoute {
   static const String locationRoute = '/location';
   static const String onboardingRoute = '/onboarding';
   static const String loginRoute = '/login';
-  static const String signupRoute = '/signup';
+  static const String signupRoute = '/register';
   static const String homeRoute = '/home';
   static const String detailsRoute = '/details';
   static const String checkoutRoute = '/checkout';
+  static const String profileRoute = '/profile';
+  static const String editProfileRoute = '/editProfile';
+  static const String favoriteRoute = '/favorite';
+  static const String createRoute = '/create';
 
   static getApplicationRoute() {
     return {
       splashRoute: (context) => const SplashScreen(),
       onboardingRoute: (context) => const OnboardingScreen(),
       loginRoute: (context) => const LoginView(),
-      homeRoute: (context) =>  const HomeScreen(),
+      homeRoute: (context) => const HomeScreen(),
       signupRoute: (context) => const SignupPage(),
-      detailsRoute: (context) =>  const DetailsPage(),
-      checkoutRoute: (context) =>  const CheckoutPage(),
+      detailsRoute: (context) => const DetailsPage(),
+      checkoutRoute: (context) => const CheckoutPage(),
+      profileRoute: (context) => const UserProfile(),
+      editProfileRoute: (context) => const EditProfilePage(),
+      favoriteRoute: (context) => const FavoritesPage(),
+      createRoute: (context) => const CreateEventPage()
     };
-
   }
 }
-
