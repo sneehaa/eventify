2e00bfa5657e42a6bc4cd13aedffa7b324211395
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-06-29 14:33:45 +0545

    updating invite_banner.dart

--- lib/features/home/widgets/invite_banner.dart
+++ lib/features/home/widgets/invite_banner.dart
@@ -6,31 +6,31 @@ class InviteBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 30),
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
       child: Container(
-        width: 391,
-        height: 134,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/invite.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: MediaQuery.of(context).size.height * 0.15,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/invite.png'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(5)),
         child: Stack(
           children: [
             Positioned(
-              right: 80,
-              top: 85,
+              right: 72,
+              top: 83,
               width: 100,
               height: 30,
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, 
-                  backgroundColor: Colors.white, 
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
                   shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
-              ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
                 ),
                 child: const Text('Invite'),
               ),
