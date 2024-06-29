a7045e8bc06e01cb43fa1f7be7e330f4cd4a7ce2
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-06-29 14:33:29 +0545

    Updated onboarding_screen.dart

--- lib/features/onBoarding/onboarding_screen.dart
+++ lib/features/onBoarding/onboarding_screen.dart
@@ -15,78 +15,81 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: Stack(
         children: [
           Positioned(
-            left: 10,
-            top: 174,
-            width: 408,
-            height: 292,
+            left: 8,
+            top: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.width * 0.95,
+            height: MediaQuery.of(context).size.height * 0.3,
             child: Image.asset("assets/images/onboarding.png"),
           ),
-          const Positioned(
-            left: 19,
-            top: 573,
-            width: 393,
-            height: 59,
-            child: Text(
+          Positioned(
+            left: MediaQuery.of(context).size.width * 0.05,
+            top: MediaQuery.of(context).size.height * 0.65,
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: MediaQuery.of(context).size.height * 0.08,
+            child: const Text(
               'Discover, Create, Attend - Turn your event ideas into unforgettable experiences',
               textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 15),
             ),
           ),
           Positioned(
-            left: -8,
-            top: 791,
-            width: 446,
-            height: 141,
+            left: 0,
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.15,
             child: Container(
               color: const Color(0xFFFFF0BC),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 20),
-                  Container(
-                    width: 14,
-                    height: 14,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF8CC8B0),
-                      shape: BoxShape.circle,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 20),
+                    Container(
+                      width: 10,
+                      height: 10,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF8CC8B0),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 8),
-                  Container(
-                    width: 10,
-                    height: 10,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF8CC8B0),
-                      shape: BoxShape.circle,
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 8,
+                      height: 8,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF8CC8B0),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 8),
-                  Container(
-                    width: 8,
-                    height: 8,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF8CC8B0),
-                      shape: BoxShape.circle,
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 6,
+                      height: 6,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF8CC8B0),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 8), 
-                  Container(
-                    width: 6,
-                    height: 6,
-                    decoration: const BoxDecoration(
-                      color:  Color(0xFF8CC8B0),
-                      shape: BoxShape.circle,
+                    const SizedBox(width: 8),
+                    Container(
+                      width: 5,
+                      height: 5,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF8CC8B0),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
           Positioned(
-            left: 277,
-            top: 826,
-            width: 115,
-            height: 42,
+            left: MediaQuery.of(context).size.width * 0.7,
+            bottom: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.width * 0.25,
+            height: MediaQuery.of(context).size.height * 0.035,
             child: ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, AppRoute.loginRoute);
