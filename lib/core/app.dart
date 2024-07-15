0e061256828d878fe41359f9d139a302d446fa46
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-15 11:54:01 +0545

    khalti initialization

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,28 +1,43 @@
 import 'package:eventify/config/router/app_router.dart';
+import 'package:eventify/features/details/ticket_count.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Eventify',
-        theme: ThemeData(
-        textTheme: GoogleFonts.libreBaskervilleTextTheme(
-          Theme.of(context).textTheme,
-        ).copyWith(
-          bodyLarge: GoogleFonts.libreBaskerville(),
-          bodyMedium: GoogleFonts.libreBaskerville(),
-        ),
-      ),
-        initialRoute: AppRoute.splashRoute,
-        routes: AppRoute.getApplicationRoute(),
-      ),
+    return KhaltiScope(
+      publicKey: "test_public_key_242aa7bbaf1742539c8a51ee00aba0e2",
+      enabledDebugging: true,
+      builder: (context, navKey) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => TicketCounts()),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Eventify',
+            theme: ThemeData(
+              textTheme: GoogleFonts.libreBaskervilleTextTheme(
+                Theme.of(context).textTheme,
+              ).copyWith(
+                bodyLarge: GoogleFonts.libreBaskerville(),
+                bodyMedium: GoogleFonts.libreBaskerville(),
+              ),
+            ),
+            localizationsDelegates: const [
+              KhaltiLocalizations.delegate,
+            ],
+            initialRoute: AppRoute.splashRoute,
+            routes: AppRoute.getApplicationRoute(),
+            navigatorKey: navKey,
+          ),
+        );
+      },
     );
   }
 }
