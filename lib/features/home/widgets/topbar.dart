b19e2504446d16181fedcaf106693565eadac2c7
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-15 11:59:58 +0545

    Updated topbar widget

--- lib/features/home/widgets/topbar.dart
+++ lib/features/home/widgets/topbar.dart
@@ -1,8 +1,76 @@
+import 'package:eventify/config/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
-
-class TopBar extends StatelessWidget {
+class TopBar extends StatefulWidget {
   const TopBar({super.key});
+
+  @override
+  _TopBarState createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> {
+  String currentLocation = 'Fetching location...';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLocation();
+  }
+
+  Future<void> fetchLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        currentLocation = 'Location services are disabled.';
+      });
+      return;
+    }
+
+    // Check permission status
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          currentLocation = 'Location permissions are denied.';
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        currentLocation =
+            'Location permissions are permanently denied, we cannot request permissions.';
+      });
+      return;
+    }
+
+    // Fetch position
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    // Fetch location name
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    Placemark placemark = placemarks[0];
+    String placeName = placemark.name ?? '';
+    String capitalCity = placemark.administrativeArea ?? '';
+
+    setState(() {
+      currentLocation = '$placeName, $capitalCity';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +78,34 @@ class TopBar extends StatelessWidget {
       width: double.infinity,
       height: 132,
       color: const Color(0xFFFFF0BC),
-      child:  Stack(
+      child: Stack(
         children: [
           Positioned(
             left: 21,
             top: 62,
-            child: Image.asset('assets/icons/search.png', width: 26, height: 26,)
+            child: GestureDetector(
+              onTap: () {},
+              child: Image.asset(
+                'assets/icons/search.png',
+                width: 26,
+                height: 26,
+              ),
+            ),
           ),
           Positioned(
             right: 21,
             top: 62,
-           child: Image.asset('assets/icons/notification.png', width: 26, height: 26,)
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacementNamed(
+                    context, AppRoute.notificationRoute);
+              },
+              child: Image.asset(
+                'assets/icons/notification.png',
+                width: 26,
+                height: 26,
+              ),
+            ),
           ),
           const Positioned(
             left: 131,
@@ -33,12 +118,12 @@ class TopBar extends StatelessWidget {
               ),
             ),
           ),
-          const Positioned(
+          Positioned(
             left: 126,
             top: 87,
             child: Text(
-              'Dillibazar, Kathmandu',
-              style: TextStyle(
+              currentLocation,
+              style: const TextStyle(
                 color: Colors.black,
                 fontSize: 16,
               ),
