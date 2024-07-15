bee99bad45af92bb32dc3ed96b67171e782d561d
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-15 11:48:58 +0545

    added routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,9 +1,16 @@
+import 'package:eventify/features/createEvent/bookVenue.dart';
 import 'package:eventify/features/createEvent/create_event.dart';
+import 'package:eventify/features/createEvent/myevents.dart';
 import 'package:eventify/features/details/checkout.dart';
 import 'package:eventify/features/details/details_screen.dart';
+import 'package:eventify/features/details/ticket_count.dart';
 import 'package:eventify/features/favorites/favorites.dart';
+import 'package:eventify/features/forgotPassword/forgot_password.dart';
+import 'package:eventify/features/forgotPassword/new_password.dart';
+import 'package:eventify/features/forgotPassword/verify_otp.dart';
 import 'package:eventify/features/home/home_screen.dart';
 import 'package:eventify/features/login/login.dart';
+import 'package:eventify/features/notifications/notifications.dart';
 import 'package:eventify/features/onBoarding/onboarding_screen.dart';
 import 'package:eventify/features/profile/edit_profile.dart';
 import 'package:eventify/features/profile/user_profile.dart';
@@ -25,6 +32,12 @@ class AppRoute {
   static const String editProfileRoute = '/editProfile';
   static const String favoriteRoute = '/favorite';
   static const String createRoute = '/create';
+  static const String venueRoute = '/bookvenue';
+  static const String forgotPasswordRoute = '/forgotPassword';
+  static const String verifyOtpRoute = '/verifyOtp';
+  static const String createNewPasswordRoute = '/createNewPassword';
+  static const String myeventsRoute = '/myevents';
+  static const String notificationRoute = '/notification';
 
   static getApplicationRoute() {
     return {
@@ -33,12 +46,23 @@ class AppRoute {
       loginRoute: (context) => const LoginView(),
       homeRoute: (context) => const HomeScreen(),
       signupRoute: (context) => const SignupPage(),
-      detailsRoute: (context) => const DetailsPage(),
-      checkoutRoute: (context) => const CheckoutPage(),
+      detailsRoute: (context) => const DetailsPage(
+            eventId: '',
+          ),
+      checkoutRoute: (context) => CheckoutPage(
+            event: const {},
+            ticketCounts: TicketCounts(),
+          ),
       profileRoute: (context) => const UserProfile(),
       editProfileRoute: (context) => const EditProfilePage(),
       favoriteRoute: (context) => const FavoritesPage(),
-      createRoute: (context) => const CreateEventPage()
+      createRoute: (context) => const CreateEventPage(),
+      venueRoute: (context) => const VenueBookingPage(),
+      forgotPasswordRoute: (context) => const ForgotPasswordPage(),
+      verifyOtpRoute: (context) => const VerifyOtpPage(),
+      createNewPasswordRoute: (context) => const NewPasswordPage(),
+      myeventsRoute: (context) => const MyEventsPage(),
+      notificationRoute: (context) => const NotificationsPage(),
     };
   }
 }
